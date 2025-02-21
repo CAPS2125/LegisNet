bd66ed9c956c1147c1eba08dbacf5e05cfe757a7
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-21 11:23:02 -0800

    Update to Style testing.py

--- testing.py
+++ testing.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 st.title("Testing Title")
